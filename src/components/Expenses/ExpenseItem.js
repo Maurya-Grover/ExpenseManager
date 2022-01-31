5aee8c53a5397db9f67d8c994f32b3f74becaa09
Maurya-Grover <maurya.grover@gmail.com>
2022-01-31 13:16:27 +0530

    Improve Component File Organisation

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+function ExpenseItem(props) {
+	return (
+		<Card className="expense-item">
+			{/*
+				Card is a custom component. CSS class names are supported with HTML elements not custom components.
+				Custom components only support what we code them to support.
+				therefore to ensure we can set classnames on the custom components and has an effect.
+				Hence we want to give classnames to Highest HTML tag within the Card that we want to 
+				give to Card as well with the props.className approach
+			*/}
+			<div>
+				<ExpenseDate date={props.date} />
+			</div>
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+			</div>
+			<div className="expense-item__price">${props.amount}</div>
+		</Card>
+	);
+}
+
+export default ExpenseItem;
