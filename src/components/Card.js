b36d742a4697448f616939629d802783e1ad1cb1
Maurya-Grover <maurya.grover@gmail.com>
2022-01-31 12:50:57 +0530

    Add Card Wrapper Component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,8 @@ import './Card.css';
 
 function Card(props) {
 	const classes = 'card ' + props.className;
-
+	// value of this props.children will always be the content that exists between the opening and closing
+	// tag of this custom component itself, wherever it is used.
 	return <div className={classes}>{props.children}</div>;
 }
 
